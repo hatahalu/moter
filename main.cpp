27fc1a610c0135ac1b53174c64e5bc595b7887c1
hatahalu <123358897+hatahalu@users.noreply.github.com>
2024-01-12 19:44:11 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,35 @@ using namespace std;
 
 DigitalOut inA(A0);
 DigitalOut inB(A1);
-
 PwmOut pwm(PA_3);
-
 int main(){
-    int i;
+    pwm.period_us(50);
     inA = 0;
     inB = 1;
     pwm = 1;
-
-    while(i<=1000000){
-        i++;
-        inA =!inA;
-        inB =!inB;
-
-        ThisThread::sleep_for(1s);
-    }
-    return 0;
-
 }
+// #include "mbed.h"
+// using namespace std;
+
+// DigitalOut inA(A0);
+// DigitalOut inB(A1);
+
+// PwmOut pwm(PA_3);
+
+// int main(){
+//     pwm.period_us(50);
+
+//     int i;
+//     inA = 0;
+//     inB = 1;
+//     pwm = 1;
+
+//     while(i<=1000000){
+//         i++;
+//         inA =!inA;
+//         inB =inB;
+//         ThisThread::sleep_for(1s);
+//     }
+//     return 0;
+
+// }
