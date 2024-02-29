803a1ff400f01aba71e43b8fcb99ef6dcfb0095f
hatahalu <123358897+hatahalu@users.noreply.github.com>
2024-02-29 17:13:20 +0900

    Update BrushlessMotor.cpp

--- BrushlessMotor.cpp
+++ BrushlessMotor.cpp
@@ -1,15 +1,27 @@
 #include "mbed.h"
-using namespace std;
 
-DigitalOut inA(A0);
-DigitalOut inB(A1);
-PwmOut pwm(PA_3);
+DigitalOut inA(A4); //dirのピン
+PwmOut pwm(PA_10);   //pwmのピン
 int main(){
     pwm.period_us(50);
-    inA = 0;
-    inB = 1;
-    pwm = 1;
+    inA = 0; //dir 範囲 0 or 1
+    pwm = 1;// pwm 範囲 0 から 1 , 0で止まる
 }
+
+// #include "mbed.h"
+// using namespace std;
+
+// DigitalOut inA(A0);
+// DigitalOut inB(A1);
+// PwmOut pwm(PA_3);
+// int main(){
+//     pwm.period_us(50);
+//     inA = 0;
+//     inB = 1;
+//     pwm = 1;
+// }
+
+
 // #include "mbed.h"
 // using namespace std;
 
