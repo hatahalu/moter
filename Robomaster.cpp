4a1be107488765559044ad9b17a21674a4af5056
hatahalu <123358897+hatahalu@users.noreply.github.com>
2024-12-23 23:36:04 +0900

    Update Robomaster.cpp

--- Robomaster.cpp
+++ Robomaster.cpp
@@ -54,3 +54,66 @@ int main() {
         led = !led; // 動作確認
     }
 }
+
+
+
+
+
+#include "mbed.h"
+#include "CAN.h"
+
+// モーターIDが1~4まで対応（5~8は
+
+CAN can(D10, D2); // CAN RX, CAN TX
+DigitalOut led(LED1);
+
+
+void send_motor_command(int16_t current,int motor_sed_id) {
+    int adjust_id  = ((motor_sed_id) >= 5 ? (motor_sed_id) - 4 : (motor_sed_id));
+    int high_value = (((adjust_id) - 1) * 2);
+    int low_value  = (high_value + 1);
+    // 送るモーターID
+    int motor_cmd_id = 0x200 - motor_sed_id;
+
+    char data[8] = {0};
+    // data[high_value] = current >> 8;  // 上位バイト
+    // data[low_value] = current & 0xFF; // 下位バイト
+    data[high_value] = (current&0X00FF) >> 8;  // 上位バイト
+    data[low_value] = current&0X00FF; // 下位バイト
+    CANMessage msg(motor_cmd_id, data, 8);
+    if (can.write(msg)) {
+        printf("Motor command sent: %d  ", current);
+    } else {
+        printf("Error sending motor command.\n");
+    }
+}
+
+void parse_feedback(const CANMessage &msg,int motor_rev_id) {
+    // 受け取るモーターID
+    int motor_feedmack_id = 0x200 + motor_rev_id;
+
+    if (msg.id == motor_feedmack_id) {
+        int16_t rotor_angle = (msg.data[0] << 8) | msg.data[1];
+        int16_t rotational_speed = (msg.data[2] << 8) | msg.data[3];
+        int16_t torque_current = (msg.data[4] << 8) | msg.data[5];
+        int8_t temperature = msg.data[6];
+        printf("Feedback ID: %d 角度: %d, 回転速度: %d, トルク電流: %d, 温度: %d\n",
+               motor_rev_id, rotor_angle, rotational_speed, torque_current, temperature);
+    }
+}
+
+int main() {
+    printf("スタート\n");
+
+    can.frequency(1000000); // CANの速度は1 Mbps
+
+    // Main loop
+    while (true) {
+        CANMessage msg;
+        send_motor_command(1000,6); // 送る電流値
+        if (can.read(msg)) {
+            parse_feedback(msg,6);
+        }
+        led = !led; // 動作確認
+    }
+}
